b487fa5825cfcb1946ceffcfc71fcc1479eb6dfa
tomgu1991 <guzuxing@sina.com>
2026-02-11 15:49:55 +0800

    Feat: add javaparser init

---
+++ java/ZG_CodeAnalysis4Java/src/main/java/gu/zuxing/UseJavaParser.java
@@ -0,0 +1,17 @@
+package gu.zuxing;
+
+import gu.zuxing.javaparser.VoidVisitorStarter;
+
+public class UseJavaParser {
+
+    public static void main(String[] args) {
+        // 这里可以添加使用 JavaParser 的示例代码
+        System.out.println("JavaParser demo");
+        try {
+            VoidVisitorStarter.main(args);
+        } catch (Exception e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+    }
+}
---
+++ java/ZG_CodeAnalysis4Java/src/main/java/gu/zuxing/javaparser/VoidVisitorStarter.java
@@ -0,0 +1,47 @@
+package gu.zuxing.javaparser;
+
+import com.github.javaparser.StaticJavaParser;
+import com.github.javaparser.ast.CompilationUnit;
+import com.github.javaparser.ast.body.MethodDeclaration;
+import com.github.javaparser.ast.visitor.VoidVisitor;
+import com.github.javaparser.ast.visitor.VoidVisitorAdapter;
+
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.List;
+
+public class VoidVisitorStarter {
+    private static final String FILE_PATH =
+            "src/test/resources/ReversePolishNotation.java";
+
+        private static class MethodNamePrinter extends VoidVisitorAdapter<Void> {
+
+        @Override
+        public void visit(MethodDeclaration md, Void arg) {
+            super.visit(md, arg);
+            System.out.println("Method Name Printed: " + md.getName());
+        }
+    }
+
+    private static class MethodNameCollector extends VoidVisitorAdapter<List<String>> {
+
+        @Override
+        public void visit(MethodDeclaration md, List<String> collector) {
+            super.visit(md, collector);
+            collector.add(md.getNameAsString());
+        }
+    }
+
+    public static void main(String[] args) throws Exception {
+
+        CompilationUnit cu = StaticJavaParser.parse(Files.newInputStream(Paths.get(FILE_PATH)));
+
+        VoidVisitor<Void> methodNameVisitor = new MethodNamePrinter();
+        methodNameVisitor.visit(cu, null);
+        List<String> methodNames = new ArrayList<>();
+        VoidVisitor<List<String>> methodNameCollector = new MethodNameCollector();
+        methodNameCollector.visit(cu, methodNames);
+        methodNames.forEach(n -> System.out.println("Method Name Collected: " + n));
+    }
+}
